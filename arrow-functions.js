0801fbb33dd547c8820ff231d6ef1957e90b5820
Amy Norfolk Tate <amynorfolk2@gmail.com>
2025-03-18 14:42:53 +0000

    arrow junctions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,30 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(4, 6);
+console.log(sum2);
+
+// const addTwoNumbers2 = (a, b) => (a + b); also valid 
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething = ('hey');
 
 // Returning Multiple Lines
